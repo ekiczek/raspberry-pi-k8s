217ef49737ad93d4c2266cbf6d50107b8fa3c95f
Ethan Kiczek <ethan@sickpig.com>
2023-03-08 16:01:21 -0500

    Pin K8s to 1.23.17-00 due to issues with newer versions.

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,39 +8,40 @@
 sudo apt install -y docker.io
 
 # Create or replace the contents of /etc/docker/daemon.json to enable the systemd cgroup driver
-sudo cat > /etc/docker/daemon.json <<EOF
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
-}
-EOF
+# sudo cat > /etc/docker/daemon.json <<EOF
+# {
+#   "exec-opts": ["native.cgroupdriver=systemd"],
+#   "log-driver": "json-file",
+#   "log-opts": {
+#     "max-size": "100m"
+#   },
+#   "storage-driver": "overlay2"
+# }
+# EOF
 
 # Append the cgroups and swap options to the kernel command line
 # Note the space before "cgroup_enable=cpuset", to add a space after the last existing item on the line
-sudo sed -i '$ s/$/ cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1 swapaccount=1/' /boot/firmware/cmdline.txt
+# sudo sed -i '$ s/$/ cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1 swapaccount=1/' /boot/firmware/cmdline.txt
 
 # Enable net.bridge.bridge-nf-call-iptables and -iptables6
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-EOF
-sudo sysctl --system
+# cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+# net.bridge.bridge-nf-call-ip6tables = 1
+# net.bridge.bridge-nf-call-iptables = 1
+# EOF
+# sudo sysctl --system
 
 # Add the packages.cloud.google.com atp key
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 # Add the Kubernetes repo
-cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
+# cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+# deb https://apt.kubernetes.io/ kubernetes-xenial main
+# EOF
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 
 # Update the apt cache and install kubelet, kubeadm, and kubectl
 # (Output omitted)
-sudo apt update && sudo apt install -y kubelet kubeadm kubectl
+sudo apt update && sudo apt install -y kubelet=1.23.17-00 kubeadm=1.23.17-00 kubectl=1.23.17-00
 
 # Disable (mark as held) updates for the Kubernetes packages
 sudo apt-mark hold kubelet kubeadm kubectl
